3003bda49ecdce0650a0afda10da9894bac6e146
Smarty_SP <10269846+programmerSP@users.noreply.github.com>
2019-05-27 16:34:32 +0530

    Update example.py

--- example.py
+++ example.py
@@ -2,3 +2,17 @@ def marine_lane():
     print("Marine-Lane")
 
 marine_lane()
+
+
+NOMINATION FORM
+
+PS Number	Name – Last Name	Total Years of Experience
+10636266	Soumen Patra	3 years
+10636177	Abhinay Muthyapu	3 Years
+10652833	Yadnesh Gharat	6 years
+10642188	Abhishar Mishra	6 years
+
+2 -> Team Skills & Expertise : Abinitio, Oracle, Unix, HTML, CSS, Javascript, Java, EXTjs, Hive, Spark, Kafka, Python, NodeJs
+
+3-> Our Team is best suited to solve challenges of future because We already have the  New technology expertise needed to face different obstacles and complete same in time duration. We are not only close to ETL ,Big data Technologies but also aware of how a system thrives using Backend ( Unix, Oracle, Python, etc) and Frontend(UI, Javascript, EXTJs).
+
